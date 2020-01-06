0f0c04f44db099e729f0e706ac7353efd0510ec8
Miguel Rodriguez - Compass Group <mrodriguezo@compassgroup.cl>
2020-01-06 10:10:20 -0600

    new bash stuff

---
+++ bash/create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir -p -- 01Enero
+mkdir -p -- 02Febrero
+mkdir -p -- 03Marzo
+mkdir -p -- 04Abril
+mkdir -p -- 05Mayo
+mkdir -p -- 06Junio
+mkdir -p -- 07Julio
+mkdir -p -- 08Agosto
+mkdir -p -- 09Septiembre
+mkdir -p -- 10Octubre
+mkdir -p -- 11Noviembre
+mkdir -p -- 12Diciembre
+echo "Folders created."
